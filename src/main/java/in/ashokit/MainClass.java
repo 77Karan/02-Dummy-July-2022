bd88f4f29aa7abdcc23ebab23fbd409092fa3860
karankumar <kkarankumar890@gmail.com>
2022-11-30 18:15:25 +0530

    First Commit

---
+++ src/main/java/in/ashokit/MainClass.java
@@ -0,0 +1,12 @@
+package in.ashokit;
+
+public class MainClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		System.out.println("Maven Builds a project based on pom.xml and plugins");
+		
+	}
+
+}
